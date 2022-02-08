cfa99b6a7400c2f2e0d351caa12bdb367a30682f
yjo <yjo9515@naver.com>
2022-02-08 15:00:20 +0900

    window

--- watchbook2/lib/member.dart
+++ watchbook2/lib/member.dart
@@ -156,22 +156,26 @@ class MemberState extends State<Member> {
         response.headers.addAll(headers);
         print(result[0].displayName);
         print(_contactMaps[0]['phones'][0]['value']);
-      for(int i = 0; i < result.length; i++) {
-        response.fields["cart[${i}]"] = "${i}";
-        response.fields["name[${i}]"] = "${result[i].displayName}";
-        if(_contactMaps[i]['phones'][0]['value'] != null && _contactMaps[i]['phones'][0]['value'].length != 0 && _contactMaps[i]['phones'][0]['value'].isNotEmpty){
-          response.fields["handphone[${i}]"] = "${_contactMaps[i]['phones'][0]['value']}";
-          // (base64.encode(result[i].avatar) == null || base64.encode(result[i].avatar) == '')
-          //     ? null :
-          // response.fields["picture[data][${i}]"] = "${base+base64.encode(result[i].avatar)}";
-        }
-      }
+        setState(() {
+          for(int i = 0; i < result.length; i++) {
+            response.fields["cart[${i}]"] = "${i}";
+            response.fields["name[${i}]"] = "${result[i].displayName}";
+            if(_contactMaps[i]['phones'][0]['value'] != null && _contactMaps[i]['phones'][0]['value'].length != 0 && _contactMaps[i]['phones'][0]['value'].isNotEmpty){
+              response.fields["handphone[${i}]"] = "${_contactMaps[i]['phones'][0]['value']}";
+              // (base64.encode(result[i].avatar) == null || base64.encode(result[i].avatar) == '')
+              //     ? null :
+              // response.fields["picture[data][${i}]"] = "${base+base64.encode(result[i].avatar)}";
+            }
+          }
+          chk = true;
+        });
+
       var send = await response.send();
       String res = await send.stream.bytesToString();
       print(res);
       print(response.headers);
       setState(() {
-        chk = true;
+
       });
     }catch(e){
       print(e);
