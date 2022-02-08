4aa4ea19512cc777011a5f76e8c0a2b1cd8a5cb9
yjo <yjo9515@naver.com>
2022-02-08 13:57:16 +0900

    window

--- watchbook2/lib/member.dart
+++ watchbook2/lib/member.dart
@@ -159,14 +159,10 @@ class MemberState extends State<Member> {
       for(int i = 0; i < result.length; i++) {
         response.fields["cart[${i}]"] = "${i}";
         response.fields["name[${i}]"] = "${result[i].displayName}";
-        if(_contactMaps.length > 0){
-          response.fields["handphone[${i}]"] = "${_contactMaps[i]['phones'][0]['value']}";
-        }else{
-          '';
-        }
-      // //   (base64.encode(result[i].avatar) == null || base64.encode(result[i].avatar) == '')
-      // //       ? null :
-      // //   response.fields["picture[data][${i}]"] = "${base+base64.encode(result[i].avatar)}";
+        response.fields["handphone[${i}]"] = "${_contactMaps[i]['phones'][0]['value']}";
+        (base64.encode(result[i].avatar) == null || base64.encode(result[i].avatar) == '')
+            ? null :
+        response.fields["picture[data][${i}]"] = "${base+base64.encode(result[i].avatar)}";
       }
       var send = await response.send();
       String res = await send.stream.bytesToString();
