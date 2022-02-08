e4664d50ee33aebddeba90d239eece0399517a3a
yjo <yjo9515@naver.com>
2022-02-08 11:40:30 +0900

    window

--- watchbook2/lib/member.dart
+++ watchbook2/lib/member.dart
@@ -157,14 +157,14 @@ class MemberState extends State<Member> {
         response.headers.addAll(headers);
         print(result[0].displayName);
         print(_contactMaps[0]['phones'][0]['value']);
-      for(int i = 0; i < result.length; i++) {
-        response.fields["cart[${i}]"] = "${i}";
-        response.fields["name[${i}]"] = "${result[i].displayName}";
-        response.fields["handphone[${i}]"] = "${_contactMaps[i]['phones'][0]['value']}";
-      //   (base64.encode(result[i].avatar) == null || base64.encode(result[i].avatar) == '')
-      //       ? null :
-      //   response.fields["picture[data][${i}]"] = "${base+base64.encode(result[i].avatar)}";
-      }
+      // for(int i = 0; i < result.length; i++) {
+      //   response.fields["cart[${i}]"] = "${i}";
+      //   response.fields["name[${i}]"] = "${result[i].displayName}";
+      //   response.fields["handphone[${i}]"] = "${_contactMaps[i]['phones'][0]['value']}";
+      // //   (base64.encode(result[i].avatar) == null || base64.encode(result[i].avatar) == '')
+      // //       ? null :
+      // //   response.fields["picture[data][${i}]"] = "${base+base64.encode(result[i].avatar)}";
+      // }
       var send = await response.send();
       String res = await send.stream.bytesToString();
       print(res);
