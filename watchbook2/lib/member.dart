f84c8d875ec2c936ade6f393135e4a63138dc07b
yjo <yjo9515@naver.com>
2022-02-09 17:35:08 +0900

    window

--- watchbook2/lib/member.dart
+++ watchbook2/lib/member.dart
@@ -166,8 +166,8 @@ class MemberState extends State<Member> {
                 print("${i}번 빔");
                 response.fields["handphone[${i}]"] = "";
               }else {
-                String j = _contactMaps[i]['phones'][0]['value'].value;
-                response.fields["handphone[${i}]"] = j;
+                var j = _contactMaps[i]['phones'][0]['value'].value;
+                response.fields["handphone[${i}]"] = "${j}";
                 // throw RangeError.index(result.length,'default');
                 // (base64.encode(result[i].avatar) == null || base64.encode(result[i].avatar) == '')
                 //     ? null :
