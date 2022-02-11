27f9eae40f0ed8fe633d3e5ffef7cd795e363268
yjo <yjo9515@naver.com>
2022-02-11 09:40:56 +0900

    window

--- watchbook2/lib/member.dart
+++ watchbook2/lib/member.dart
@@ -54,6 +54,8 @@ class MemberState extends State<Member> {
         return Future.value(false);
       }
     }else{
+      Map<Permission, PermissionStatus> statuses =
+      await [Permission.contacts].request();
       const snackBar =
       const SnackBar(content: Text('연락처 권한을 동의해 주세요.'));
       ScaffoldMessenger.of(context).showSnackBar(snackBar);
